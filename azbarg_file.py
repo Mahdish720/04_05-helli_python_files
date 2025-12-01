2bdaa0028b0661aa4a3a688ff93e3485c39c834a
Mahdish720 <65761297+Mahdish720@users.noreply.github.com>
2025-12-01 19:11:49 +0330

    Add files via upload

---
+++ azbarg_file.py
@@ -0,0 +1,68 @@
+# f = open("test.txt", "w") # w , a , r
+# f.write("in file baraye avvalin bar sakhte shode!!")
+# f.write(" - dare dame mitoonid baz write konid!!")
+# f.close()
+
+
+# f = open("test.txt", "r")
+# text = f.read()
+# print(text)
+# f.close()
+
+# f = open("test2.txt", "r")
+# text = f.read()
+# print(text)
+
+
+# f = open(r"C:\Users\Mahdi\Desktop\helli\04_05\codes\test2.txt", "w") 
+# f.write("first line\nsecond line\nthird line ")
+# f.close()
+
+
+# f1 = open("test.txt", "r")
+# f2 = open("test2.txt", "r")
+# lines1 = f1.readlines()
+# lines2 = f2.readlines()
+# # myinput = f2.read()
+# print(lines1)
+# print(lines2)
+# print(myinput)
+
+
+# f1 = open("test2.txt", "r")
+# text = f1.read()
+# print(text)
+
+# f = open("test2.txt", "a")
+# f.write("\nfourth line")
+# f.close()
+# f = open("test2.txt", "r")
+# print(f.readlines())
+# f.close()
+
+
+# f = open("test.txt", "w")
+# f.write("matne ghabli bayad avaz shode bashe!!")
+# f.close()
+
+
+
+#close moheme...
+#1
+# f = open("test.txt", "w")
+# f.write("salam bacheha")
+# # f.close()
+
+# f2 = open("test.txt", "r")
+# print(f2.read())
+
+# 2
+# f = open("test.txt", "w")
+# f.write("hello")
+
+
+# f2 = open("test.txt", "w")
+
+
+
+
